7318976846389a2a25b8c214ed191acdffeef736
Robert Morriss <robejmorriss@students.k12k.com>
2022-12-08 11:43:04 -0500

    add basemodel and enum classes for data types

--- database/models.py
+++ database/models.py
@@ -1,49 +1,37 @@
+
+
 from pydantic import BaseModel
+from enum import Enum
+
+class Season(Enum):
+    WINTER = 0
+    SPRING = 1
+    SUMMER = 2
+    FALL = 3
+
+class Team(Enum):
+    JV = 0
+    VARSITY = 1
+    FRESHMAN = 2
+
+class Sport(Enum):
+    M_BASKETBALL = 0 
+    W_BASKETBALL = 1 
+    BOWLING = 2
+    M_CROSS_COUNTRY = 3
+    W_CROSS_COUNTRY = 4
+    FOOTBALL = 5
+    M_GOLF = 6
+    W_GOLF = 7
+    W_SOCCER = 8
+    W_VOLLEYBALL = 9
 
 class Result(BaseModel):
-    type: str
-    description: str
-
-class GameNotes(BaseModel):
-    type: str
-    description: str
-
-class Link(BaseModel):
-    type: str
-    description: str
-
-class OurScore(BaseModel):
-    type: int
-    description: int
-
-class TheirScore(BaseModel):
-    type: int
-    description: int
-
-class Team(BaseModel):
-    type: enumerate
-    description: enumerate
-
-class Sport(BaseModel):
-    type: enumerate
-    description: enumerate
-
-class Season(BaseModel):
-    type: enumerate
-    description: enumerate
-
-class Date(BaseModel):
-    type: str
-    description: str
-
-class Opponents(BaseModel):
-    type: str
-    description: str
-
-class Location(BaseModel):
-    type: str
-    description: str
-
-class Identification(BaseModel):
-    type: str
-    description: str
+    our_score: int
+    their_score: int
+    game_notes: str
+    result: str
+    link: str
+    season: Season
+    team: Team
+    sport: Sport
